af6da711eeda52eb0cff3cc9462cc6a13e0810e4
Dell <Dell@KUSHI>
2024-07-16 13:38:15 +0530

    First commit

---
+++ src/test/java/TestScripts/Test3.java
@@ -0,0 +1,13 @@
+package TestScripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test3
+{
+ @Test
+ public void demo() 
+ {
+	 Reporter.log("Test3",true);
+ }
+}
